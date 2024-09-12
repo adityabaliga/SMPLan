0d03af14cd0921ca2657e3b5e68e91f75b4eda3f
adityabaliga <aditya.baliga@gmail.com>
2024-09-12 09:53:50 +0530

    Changed allowance for submitting processing

--- static/js/processing.js
+++ static/js/processing.js
@@ -651,9 +651,9 @@ function validate(){
     // If RM weight is more then the check of 7% is ok. But when RM weight is less, then  5% margin becomes less
     // So, when RM less than 3MT, I am giving an allowance of 8%
 
-    var allowance = 1.01;
+    var allowance = 1.02;
     if(rm_wt < 3){
-        allowance = 1.02;
+        allowance = 1.03;
     }
 
     if((total_processed_wt + completed_proc_wt) > allowance*rm_wt){
