ee7f8bf306a93952e079d081efb12a4a12c5d976
Aditya Baliga <aditya.baliga@gmail.com>
2024-10-23 13:38:28 +0530

    Changed percentage of RM based on which RM is deleted

--- app.py
+++ app.py
@@ -1558,16 +1558,17 @@ def submit_processing():
                                 # If all the order details in that stage are complete, it makes the order details of the next stage ready for production
                                 # If this is the last stage of the order, it marks the order as closed
                                 # OrderDetail.detail_complete(order_detail_id)
-                    wt_factor = '0.02'
+                    wt_factor = Decimal('0.02')
                     if rm_wt < 5:
-                        wt_factor = '0.05'
+                        wt_factor = Decimal('0.07')
                     if rm_wt < 10 and rm_wt > 5:
-                        wt_factor = '0.04'
+                        wt_factor = Decimal('0.05')
                     if rm_wt < 15 and rm_wt > 10:
-                        wt_factor = '0.03'
+                        wt_factor = Decimal('0.04')
 
-                    if (new_rm_weight < (Decimal(wt_factor) * rm_wt) and Decimal(ms_length) == 0) or (
-                            (new_rm_weight < (Decimal(wt_factor) * rm_wt)) and Decimal(ms_length) > 0):
+
+                    if (new_rm_weight < ((wt_factor) * rm_wt) and Decimal(ms_length) == 0) or (
+                            (new_rm_weight < ((wt_factor) * rm_wt)) and Decimal(ms_length) > 0):
                         # OrderDetail.complete_processing_on_del(smpl_no, width, length)
                         # CurrentStock.delete_record(cs_id)
 
@@ -1908,13 +1909,14 @@ def submit_slitting_processing():
                                 return render_template('/main_menu.html',
                                                        message=Markup("Entry Failed. Please check RM weight"))
 
-                            wt_factor = '0.02'
+                            wt_factor = Decimal('0.02')
                             if rm_wt < 5:
-                                wt_factor = '0.05'
+                                wt_factor = Decimal('0.07')
                             if rm_wt < 10 and rm_wt > 5:
-                                wt_factor = '0.04'
+                                wt_factor = Decimal('0.05')
                             if rm_wt < 15 and rm_wt > 10:
-                                wt_factor = '0.03'
+                                wt_factor = Decimal('0.04')
+
                             if (new_rm_weight < (Decimal(wt_factor) * rm_wt)):
                                 # OrderDetail.complete_processing_on_del(smpl_no, width, length)
                                 # CurrentStock.delete_record(cs_id)
