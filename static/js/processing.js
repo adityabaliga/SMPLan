f1895e4339efbcf8b0ebf7b415d71e176ca90966
Aditya Baliga <aditya.baliga@gmail.com>
2025-12-06 17:32:16 +0530

    Added Crystal in Lami Cos

--- static/js/processing.js
+++ static/js/processing.js
@@ -315,7 +315,7 @@ function enable_lami_co(table_id, table_row){
 	if(lami_type.includes("Single Side")){
 	    //console.log(last_row.cells[7].firstElementChild[0].innerText);
 	    //last_row.cells[7].disabled = false;
-	    last_row.cells[7].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Other">Other</option></select>';
+	    last_row.cells[7].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Crystal">Crystal</option><option value="Other">Other</option></select>';
         last_row.cells[8].innerHTML ='<td><select id = "lami_co_top" name= "lami_co_top" ><option value = "No Lami">No Lami</option></select></td>';
         //last_row.cells[7].required = true;
         //last_row.cells[8].required = false;
@@ -323,8 +323,8 @@ function enable_lami_co(table_id, table_row){
 	else if(lami_type.includes("Both Side")){
 	    //console.log(last_row.cells[7].firstElementChild[0].innerText);
 	    //last_row.cells[7].disabled = false;
-	    last_row.cells[7].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Other">Other</option></select>';
-	    last_row.cells[8].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Other">Other</option></select>';
+	    last_row.cells[7].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Crystal">Crystal</option><option value="Other">Other</option></select>';
+	    last_row.cells[8].innerHTML ='<select id="lami_co_top" name="lami_co_top"><option value="">Select</option><option value="Futura">Futura</option><option value="SVS">SVS Pack</option><option value="Paragon">Paragon</option><option value="Superbright">Superbright</option><option value="Crystal">Crystal</option><option value="Other">Other</option></select>';
         //last_row.cells[7].required = true;
         //last_row.cells[8].required = true;
 	}
